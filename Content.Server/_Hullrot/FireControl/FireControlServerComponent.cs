fd737e997f0127d748e07057fb614f7ee8aec02d
Rane <elijahrane@gmail.com>
2025-01-15 18:47:28 -0500

    notes for tomorrow

---
+++ Content.Server/_Hullrot/FireControl/FireControlServerComponent.cs
@@ -0,0 +1,15 @@
+namespace Content.Server._Hullrot.FireControl;
+
+/// My fire control system replaces the functionality of point cannons.
+/// I'll keep my notes here on how it works.
+/// 1) Ships have a central fire control server that keeps track of the available remote weapons (turrets, drones, etc)
+/// 2) The server leases control of these weapons to targeting consoles, etc, allowing multiple targeting consoles on a ship
+/// 3) Targeting consoles generally have RTS style selection controls in addition to tags
+/// 4) Control groups from that are not diegetic and lets players personalize them
+
+
+[RegisterComponent]
+public sealed partial class FireControlServerComponent : Component
+{
+
+}
