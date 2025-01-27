92ede916a80cc0e1ef9208ca6e1e7d7567b14d76
Rane <elijahrane@gmail.com>
2025-01-27 18:15:13 -0500

    scaffolding for UI state

--- Content.Server/_Hullrot/FireControl/FireControlServerComponent.cs
+++ Content.Server/_Hullrot/FireControl/FireControlServerComponent.cs
@@ -14,4 +14,7 @@ public sealed partial class FireControlServerComponent : Component
 
     [ViewVariables]
     public HashSet<EntityUid> Consoles = new();
+
+    [ViewVariables]
+    public Dictionary<EntityUid, EntityUid> Leases;
 }
--- Content.Shared/_Hullrot/FireControl/FireControlMessages.cs
+++ Content.Shared/_Hullrot/FireControl/FireControlMessages.cs
@@ -1,4 +1,5 @@
 using Robust.Shared.Serialization;
+using Robust.Shared.Map;
 
 namespace Content.Shared._Hullrot.FireControl;
 
@@ -7,6 +8,19 @@ public sealed class FireControlConsoleUpdateEvent : EntityEventArgs
 {
 }
 
+/// <summary>
+/// Kind of modeled these off of how air alarms handle all this.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class FireControlConsoleBoundInterfaceState : BoundUserInterfaceState
+{
+    public FireControllableEntry[] FireControllables;
+
+    public FireControlConsoleBoundInterfaceState(FireControllableEntry[] fireControllables)
+    {
+        FireControllables = fireControllables;
+    }
+}
 
 [Serializable, NetSerializable]
 public enum FireControlConsoleUiKey : byte
@@ -18,4 +32,30 @@ public enum FireControlConsoleUiKey : byte
 public sealed class FireControlConsoleRefreshServerMessage : BoundUserInterfaceMessage
 {
 
+}
+
+[Serializable, NetSerializable]
+public struct FireControllableEntry
+{
+    /// <summary>
+    /// The entity in question
+    /// </summary>
+    public NetEntity NetEntity;
+
+    /// <summary>
+    /// Location of the entity
+    /// </summary>
+    public NetCoordinates Coordinates;
+
+    /// <summary>
+    /// Display name of the entity
+    /// </summary>
+    public string Name;
+
+    public FireControllableEntry(NetEntity entity, NetCoordinates coordinates, string name)
+    {{
+        NetEntity = entity;
+        Coordinates = coordinates;
+        Name = name;
+    }}
 }
